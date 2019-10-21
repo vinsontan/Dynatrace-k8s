dcf85d83c5c4d92e6dc9ba3ce27c51a7b476718e
vinsontan <vinson.tan@dynatrace.com>
2019-10-21 17:57:23 +1100

    Update setup-activegate.sh

--- setup-activegate.sh
+++ setup-activegate.sh
@@ -7,7 +7,7 @@ gcloud compute instances create dynatrace-activegate \
 --image-project ubuntu-os-cloud \
 --zone australia-southeast1-a \
 
-sleep 10 secs
+sleep 10s
 
 gcloud compute scp ~/Dynatrace-k8s/install-activegate.sh \
       dynatrace-activegate:~/ --zone australia-southeast1-a
